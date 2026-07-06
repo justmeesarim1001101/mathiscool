6fa267ef69d08b60fffc940cc26bbcd5e6151583
justmeesarim1001101 <justmeesarim1001101@gmail.com>
2026-07-06 14:51:46 -0400

    Add calculator function with operations and secret code
    Implement a calculator function with basic operations and a secret code feature.

---
+++ index.js
@@ -0,0 +1,30 @@
+function calc(){
+let NumOne = Number(document.getElementById("no1").value);
+let NumTwo = Number(document.getElementById("no2").value);
+let Operation = document.getElementById("operation").value;
+let Answer = document.getElementById("ans");
+let Equals;
+switch(Operation){
+  case "+":
+  Equals = NumOne + NumTwo;
+  break;
+  case "-":
+  Equals = NumOne - NumTwo;
+  break;
+  case "*":
+  Equals = NumOne * NumTwo;
+  break;
+  case "/":
+  Equals = NumOne / NumTwo;
+  break;
+  case "SEcREt_code_123M":
+  Equals = "So you found the secret code... The password is 1001101!"
+  window.open('mathiscool.github.io/funthingstodo.html', '_blank');
+  break;
+}
+if(Operation == "SEcREt_code_123M"){
+Answer.innerText = String(Equals);
+}else{
+  Answer.innerText = "It's equal to " + String(Equals);
+}
+}
